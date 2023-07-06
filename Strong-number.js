f273a13973464e4f39a5133f22bb0126425c4ff2
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-07-05 17:13:25 -0700

    Update Strong-number.js

--- Strong-number.js
+++ Strong-number.js
@@ -24,5 +24,18 @@ is equal to number itself, then its a Strong.
 
 // Solution
 
-let arr = String(n).split("");
-let sum = 0;
+function strong(n) {
+  let arr = String(n).split("");
+  let sum = 0;
+  function factorial(fnNum) {
+    if(fnNum <= 1) {
+      return 1;
+    } else {
+      return fnNum * factorial(fnNum - 1);
+    }
+  }
+  for (let i = 0; i < arr.length; i++) {
+    sum += factorial(arr[i]);
+  }
+  return n == sum ? "STRONG!!!!" : "Not Strong!!";
+}
